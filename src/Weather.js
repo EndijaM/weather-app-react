06ac434d9587c00f1c6921297df5270694fb6638
EndijaM <115448772+EndijaM@users.noreply.github.com>
2022-12-22 13:07:38 +0100

    Created functions for city and weather data

--- src/Weather.js
+++ src/Weather.js
@@ -1,50 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./Weather.css";
 
 export default function Weather(props) {
-  function handleResponse(response) {
-    return (
-      <h3>
-        The weather in ${response.data.name} is $
-        {Math.round(response.data.main.temp)}°C
-      </h3>
-    );
+  const [city, setCity] = useState(props.city);
+  const [weather, setWeather] = useState("");
+  const [update, setUpdate] = useState(false);
+
+  console.log(update);
+
+  function displayWeather(response) {
+    setWeather({
+      temperature: response.data.main.temp,
+      humidity: response.data.main.humidity,
+      wind: response.data.wind.speed,
+      description: response.data.weather[0].description,
+      icon: ` http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+    });
+    setUpdate(true);
   }
 
-  let apiKey = "99b8f9330a1bfba3a85e523fd3c2e528";
-  let units = "metric";
-  let url = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=${units}`;
-  axios.get(url).then(handleResponse);
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiKey = "99b8f9330a1bfba3a85e523fd3c2e528";
+    let units = "metric";
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+
+    if (city.length > 0) {
+      axios.get(url).then(displayWeather);
+    } else {
+      alert(`Please type a city!`);
+    }
+  }
+
+  function updateCity(event) {
+    event.preventDefault();
+    setCity(event.target.value);
+  }
 
   return (
-    <form>
-      <div className="row">
-        <div className="col-sm-8">
-          <input
-            type="search"
-            placeholder="Type a city"
-            autocomplete="off"
-            className="form-control"
-          />
+    <div className="container">
+      <form action="" onSubmit={handleSubmit}>
+        <div className="row">
+          <div className="col-sm-6 m-1">
+            <input
+              type="search"
+              placeholder="Type a city"
+              autocomplete="off"
+              className="form-control"
+            />
+          </div>
+          <div className="col-sm-3 m-1">
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-secondary w-100"
+              onChange={updateCity}
+            />
+          </div>
+          <div className="col-sm-2 m-1">
+            <input
+              type="button"
+              value="📌"
+              className="btn btn-secondary w-100"
+            />
+          </div>
         </div>
-        <div className="col-sm-2">
-          <input
-            type="submit"
-            value="Search"
-            className="btn btn-secondary w-100"
-          />
-        </div>
-        <div className="col-sm-2">
-          <input
-            type="button"
-            value="Local"
-            className="btn btn-secondary w-100"
-          />
-        </div>
-      </div>
-    </form>
+      </form>
+    </div>
   );
 }
