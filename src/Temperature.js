19e14b8ad5432bc93525dac321e8649b9c15f975
EndijaM <115448772+EndijaM@users.noreply.github.com>
2022-12-22 18:16:34 +0100

    Updated Temperature depositories

--- src/Temperature.js
+++ src/Temperature.js
@@ -3,31 +3,55 @@ import React, { useState } from "react";
 import "./Temperature.css";
 
 export default function Temperature(props) {
-  const [temperature, setTemperature] = useState(props.value);
+  const [degree, setDegree] = useState("celsius");
 
   function showCelsius(event) {
     event.preventDefault();
-    setTemperature(props.value);
+
+    setDegree("celsius");
   }
 
   function showFahrenheit(event) {
     event.preventDefault();
-    setTemperature(Math.round((props.value * 9) / 5 + 32));
+
+    setDegree("fahrenheit");
   }
 
-  return (
-    <div className="Temperature">
-      <h2>
-        {" "}
-        {temperature}°{" "}
-        <a href="/" className="Unit" onClick={showCelsius}>
-          C |{" "}
-        </a>
-        <a href="/" className="Unit" onClick={showFahrenheit}>
-          {" "}
-          F{" "}
-        </a>
-      </h2>
-    </div>
-  );
+  if (degree === "celsius") {
+    return (
+      <div className="Temperature">
+        <h2>
+          {props.value}°{" "}
+          <span className="Unit">
+            <a href="/" onClick={showCelsius} className="deactivated">
+              C{" "}
+            </a>
+            |
+            <a href="/" onClick={showFahrenheit}>
+              {" "}
+              F{" "}
+            </a>
+          </span>
+        </h2>
+      </div>
+    );
+  } else {
+    return (
+      <div className="Temperature">
+        <h2>
+          {Math.round((props.value * 9) / 5 + 32)}°{" "}
+          <span className="Unit">
+            <a href="/" onClick={showCelsius}>
+              C{" "}
+            </a>
+            |
+            <a href="/" onClick={showFahrenheit} className="deactivated">
+              {" "}
+              F{" "}
+            </a>
+          </span>
+        </h2>
+      </div>
+    );
+  }
 }
